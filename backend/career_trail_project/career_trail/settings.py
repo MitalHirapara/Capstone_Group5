7789b31bfd0811bb9ad10e9f4dd295bd8339c113
MitalHirapara <mitalhirapara1101@gmail.com>
2024-09-26 23:49:47 -0400

    separate database credential to .env file

--- backend/career_trail_project/career_trail/settings.py
+++ backend/career_trail_project/career_trail/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,11 +98,11 @@ WSGI_APPLICATION = 'career_trail.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.pwysfthxmfriuyggeegk',
-        'PASSWORD': 'Careertrail@123',
-        'HOST': 'aws-0-ca-central-1.pooler.supabase.com',
-        'PORT': '6543',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
