7e0ca24543e747d6f116c37a6d5c9c6f4f18c9ee
MitalHirapara <mitalhirapara1101@gmail.com>
2024-10-04 17:29:23 -0400

    complete Job Post
    Job Post page code with search and filter with job box

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import JobFilterHead from '../components/user/JobFilterHead'
 import JobBox from '../components/user/JobBox'
+import JobSortBar from '../components/user/JobSortBar'
 
 export default function Jobs() {
   return (
     <>
     <JobFilterHead/>
+    <JobSortBar/>
     <JobBox/>
     </>
   )
