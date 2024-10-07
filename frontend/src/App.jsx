c3a8595223293b4bc34ff156a53d96c1449b3152
DollyPatel13 <dollypatel280501@gmail.com>
2024-10-07 15:30:25 -0400

    Signup Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
+import Navbar from "./components/user/Navbar";
+import Signup from "./components/Signup"
 // import ResumeBuild from "./pages/ResumeBuild";
 import EmployerDashboard from "./pages/EmployerDashboard";
 import CreateJobPost from "./components/employer/CreateJob";
@@ -14,24 +15,24 @@ import EmployerLayout from "./layouts/EmloyerLayout";
 import Jobs from "./pages/Jobs";
 
 export default function App() {
-    return (
-        <Routes>
-            {/* Public routes - accessible by everyone */}
-            <Route element={<UserLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                {/* <Route path="/signup" element={<Signup />} /> */}
-                <Route path="/jobs" element={<Jobs />} />
-                {/* <Route path="/resume-build" element={<ResumeBuild />} /> */}
-            </Route>
+  return (
+    <Routes>
+      {/* Public routes - accessible by everyone */}
+      <Route element={<UserLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/jobs" element={<Jobs />} />
+        {/* <Route path="/resume-build" element={<ResumeBuild />} /> */}
+      </Route>
 
-            {/* Private routes - only accessible by authenticated employers */}
-            <Route element={<EmployerLayout />}>
-                <Route path="/dashboard/*" element={<EmployerDashboard />} />
-                <Route path="/create-job-post" element={<CreateJobPost />} />
-                <Route path="/manage-jobs" element={<ManageJobs />} />
-                {/* <Route path="/applications" element={<Applications />} /> */}
-            </Route>
-        </Routes>
-    );
+      {/* Private routes - only accessible by authenticated employers */}
+      <Route element={<EmployerLayout />}>
+        <Route path="/dashboard/*" element={<EmployerDashboard />} />
+        <Route path="/create-job-post" element={<CreateJobPost />} />
+        <Route path="/manage-jobs" element={<ManageJobs />} />
+        {/* <Route path="/applications" element={<Applications />} /> */}
+      </Route>
+    </Routes>
+  );
 }
