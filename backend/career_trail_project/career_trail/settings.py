ce20b26b4afe10bad7576beac65dd0bb9ba59e93
DollyPatel13 <dollypatel280501@gmail.com>
2024-12-07 19:09:47 -0500

    Update settings.py

--- backend/career_trail_project/career_trail/settings.py
+++ backend/career_trail_project/career_trail/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-uet_u-p1@0cus_e!_40=^_hw1q38-xlasu(%sxe9_(*og76nwa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'https://career-trail.onrender.com/' ]
 # Frontend URL
 FRONTEND_URL = 'http://localhost:5173'
 
@@ -39,6 +39,7 @@ FRONTEND_URL = 'http://localhost:5173'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # URL of your React app
     'http://127.0.0.1:5173',  # Handle both localhost and 127.0.0.1
+    'https://career-trail.onrender.com/'
 ]
 
 REST_FRAMEWORK = {
